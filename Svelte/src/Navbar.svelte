2c23ad849fbe32d2e9195667dc0a5d5d64525fb8
Hans <bmm79@nau.edu>
2019-06-08 15:27:55 -0700

    add bootstrap with dropdown menu

--- Svelte/src/Navbar.svelte
+++ Svelte/src/Navbar.svelte
@@ -5,9 +5,35 @@
 	const toggleControls = () => (showControls = ! showControls);
 </script>
 
-<div class="navbar bg-primary">
-  <h1>Nav</h1>
-  				<button class="btn" on:click={toggleControls}>
-					{#if showControls}x{:else}={/if}
-				</button>
+<div class="pos-f-t">
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" on:click={toggleControls} aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      {#if showControls}
+        <i class="fa fa-times"></i>
+      {:else} 
+        <span class="navbar-toggler-icon"></span>
+      {/if}
+    </button>
+  </nav>
+{#if showControls}
+    <div class=" navbar-collapse bg-dark p-4">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+{:else}
+
+{/if}
 </div>
+
+
+<style>
+.nav-link{
+  color: white;
+}
+</style>
